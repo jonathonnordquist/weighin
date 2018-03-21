8f0fab754f4446b9bfd6dd90a3984fe9e761471c
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2018-03-21 18:44:55 -0500

    Update leagues_controller.rb

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -4,6 +4,12 @@ class LeaguesController < ApplicationController
   end
 
   def show_event
+#     Be more explicit about the id params
+#     Didn't index the database
+#     N + 1 querying, nested queries, end up calling the query N times with inner query, +1 for the outer query
+#     Ruby encourges this, have to be careful about this, learn how to tell AR to preload all the required data in a single query instead of a swarm of them
+#     Run the console and be on the lookout for a ton of selects
+#     With calculating the weight could have precomputed and stored
     @event = Event.find(params[:lid])
     @league = League.find(params[:lid])
     event_people_names = @event.people.map(&:name)
